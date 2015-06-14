171890b5f48446ff0b6bb79d6f45337f72d637ac
imercer <isaac@isaacscomputertips.com>
2015-06-14 15:40:05 +1200

    Added New Locations
    Added all the new locations

--- NearlyThere/ViewController.swift
+++ NearlyThere/ViewController.swift
@@ -68,6 +68,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, ADBannerViewD
     BaseCrown.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
         -44.97681, longitude: 168.863223), radius: 1000.0, identifier: "Location1")
     BaseCrown.region.notifyOnExit = false
+    BaseCrown.soundName = "crownrange.mp3"
     UIApplication.sharedApplication().scheduleLocalNotification(BaseCrown)
     
     let Cadrona = UILocalNotification()
@@ -76,6 +77,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, ADBannerViewD
     Cadrona.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
         -44.89228, longitude: 168.994017), radius: 1000.0, identifier: "Location2")
     Cadrona.region.notifyOnEntry = false
+    Cadrona.soundName = "crownrange.mp3"
     UIApplication.sharedApplication().scheduleLocalNotification(Cadrona)
     
     let OutsideKGorge = UILocalNotification()
@@ -115,13 +117,96 @@ class ViewController: UIViewController, CLLocationManagerDelegate, ADBannerViewD
     UIApplication.sharedApplication().scheduleLocalNotification(MilfordRoad)
     
     let DevilsStaircase = UILocalNotification()
-    DevilsStaircase.alertBody = "You have entered a high risk zone (Devils Staircase) , proceed with caution. There is no fuel beyond this point."
+    DevilsStaircase.alertBody = "You have entered a high risk zone (Devils Staircase) , proceed with caution"
     DevilsStaircase.regionTriggersOnce = false
     DevilsStaircase.soundName = "devilsstaircase.mp3"
     DevilsStaircase.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
         -45.199407, longitude: 168.742583), radius: 10000.0, identifier: "Location7")
     DevilsStaircase.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(DevilsStaircase)
+    
+    let DesertRoad = UILocalNotification()
+    DesertRoad.alertBody = "You have entered a high risk zone (Desert Road) , proceed with caution."
+    DesertRoad.regionTriggersOnce = false
+    DesertRoad.soundName = "desertroad.mp3"
+    DesertRoad.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
+        -39.267437, longitude: 175.736685), radius: 25000.0, identifier: "Location8")
+    DesertRoad.region.notifyOnExit = false
+    UIApplication.sharedApplication().scheduleLocalNotification(DesertRoad)
+    
+    let HaurakiPlains = UILocalNotification()
+    HaurakiPlains.alertBody = "You have entered a high risk zone (Hauraki Plains) , proceed with caution."
+    HaurakiPlains.regionTriggersOnce = false
+    HaurakiPlains.soundName = "desertroad.mp3"
+    HaurakiPlains.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
+        -37.246316, longitude: 175.199842), radius: 12000.0, identifier: "Location9")
+    HaurakiPlains.region.notifyOnExit = false
+    UIApplication.sharedApplication().scheduleLocalNotification(HaurakiPlains)
+    
+    let KaimaiRanges = UILocalNotification()
+    KaimaiRanges.alertBody = "You have entered a high risk zone (Kaimai Ranges) , proceed with caution."
+    KaimaiRanges.regionTriggersOnce = false
+    KaimaiRanges.soundName = "kaimai.mp3"
+    KaimaiRanges.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
+        -37.825720, longitude: 175.996133), radius: 10000.0, identifier: "Location10")
+    KaimaiRanges.region.notifyOnExit = false
+    UIApplication.sharedApplication().scheduleLocalNotification(KaimaiRanges)
+    
+    let Karangahake = UILocalNotification()
+    Karangahake.alertBody = "You have entered a high risk zone (Karangahake Gorge) , proceed with caution."
+    Karangahake.regionTriggersOnce = false
+    Karangahake.soundName = "karangaheke.mp3"
+    Karangahake.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
+        -37.418901, longitude: 175.742572), radius: 3000.0, identifier: "Location11")
+    Karangahake.region.notifyOnExit = false
+    UIApplication.sharedApplication().scheduleLocalNotification(Karangahake)
+
+    let LindisPass = UILocalNotification()
+    LindisPass.alertBody = "You have entered a high risk zone (Lindis Pass) , proceed with caution."
+    LindisPass.regionTriggersOnce = false
+    LindisPass.soundName = "lindispass.mp3"
+    LindisPass.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
+        -44.585636, longitude: 169.643528), radius: 37000.0, identifier: "Location12")
+    LindisPass.region.notifyOnExit = false
+    UIApplication.sharedApplication().scheduleLocalNotification(LindisPass)
+    
+    let RimutakaHill = UILocalNotification()
+    RimutakaHill.alertBody = "You have entered a high risk zone (Rimutaka Hill Road) , proceed with caution."
+    RimutakaHill.regionTriggersOnce = false
+    RimutakaHill.soundName = "rimutakahill.mp3"
+    RimutakaHill.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
+        -41.109769, longitude: 175.253011), radius: 5000.0, identifier: "Location13")
+    RimutakaHill.region.notifyOnExit = false
+    UIApplication.sharedApplication().scheduleLocalNotification(RimutakaHill)
+    
+    let NapierTaupo = UILocalNotification()
+    NapierTaupo.alertBody = "You have entered a high risk zone (Napier-Taupo Road) , proceed with caution."
+    NapierTaupo.regionTriggersOnce = false
+    NapierTaupo.soundName = "napiertaupo.mp3"
+    NapierTaupo.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
+        -39.117149, longitude: 176.581215), radius: 30000.0, identifier: "Location14")
+    NapierTaupo.region.notifyOnExit = false
+    UIApplication.sharedApplication().scheduleLocalNotification(NapierTaupo)
+    
+    let DomeValley = UILocalNotification()
+    DomeValley.alertBody = "You have entered a high risk zone (Dome Valley) , proceed with caution."
+    DomeValley.regionTriggersOnce = false
+    DomeValley.soundName = "domevalley.mp3"
+    DomeValley.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
+        -36.361306, longitude: 174.606477), radius: 6000.0, identifier: "Location15")
+    DomeValley.region.notifyOnExit = false
+    UIApplication.sharedApplication().scheduleLocalNotification(DomeValley)
+    
+    let Brynderwyn = UILocalNotification()
+    Brynderwyn.alertBody = "You have entered a high risk zone (Brynderwyn Hills) , proceed with caution."
+    Brynderwyn.regionTriggersOnce = false
+    Brynderwyn.soundName = "brynderwyns.mp3"
+    Brynderwyn.region = CLCircularRegion(circularRegionWithCenter: CLLocationCoordinate2D(latitude:
+        -36.069030, longitude: 174.421383), radius: 2600.0, identifier: "Location16")
+    Brynderwyn.region.notifyOnExit = false
+    UIApplication.sharedApplication().scheduleLocalNotification(Brynderwyn)
+
+    
   }
     
     func bannerViewDidLoadAd(banner: ADBannerView!) {

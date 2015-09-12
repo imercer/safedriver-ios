8dfcda8427d2d57bd7847f8fb5ee15441255c007
Isaac Mercer <isaac@isaacmercer.nz>
2015-09-13 11:44:21 +1200

    Removed NotifyOnExit and Entry properties because bugs

--- NearlyThere/ViewController.swift
+++ NearlyThere/ViewController.swift
@@ -61,146 +61,146 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let BaseCrown = UILocalNotification()
     BaseCrown.alertBody = "You have entered a high risk zone (Crown Range Road) , proceed with caution"
-    BaseCrown.regionTriggersOnce = false
+    BaseCrown.regionTriggersOnce = true
     BaseCrown.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -44.97681, longitude: 168.863223), radius: 1000.0, identifier: "Location1")
-    BaseCrown.region?.notifyOnExit = false
+    //BaseCrown.region.notifyOnExit = false
     BaseCrown.soundName = "crownrange.mp3"
     UIApplication.sharedApplication().scheduleLocalNotification(BaseCrown)
     
     let Cadrona = UILocalNotification()
     Cadrona.alertBody = "You have entered a high risk zone (Crown Range Road) , proceed with caution"
-    Cadrona.regionTriggersOnce = false
+    Cadrona.regionTriggersOnce = true
     Cadrona.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -44.89228, longitude: 168.994017), radius: 1000.0, identifier: "Location2")
-    Cadrona.region?.notifyOnEntry = false
+    //Cadrona.region.notifyOnEntry = false
     Cadrona.soundName = "crownrange.mp3"
     UIApplication.sharedApplication().scheduleLocalNotification(Cadrona)
     
     let OutsideKGorge = UILocalNotification()
     OutsideKGorge.alertBody = "You have entered a high risk zone (Kawarau Gorge Road) , proceed with caution"
-    OutsideKGorge.regionTriggersOnce = false
+    OutsideKGorge.regionTriggersOnce = true
     OutsideKGorge.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -45.049776, longitude: 169.032084), radius: 1000.0, identifier: "Location3")
-    OutsideKGorge.region?.notifyOnExit = false
+    //OutsideKGorge.region.notifyOnExit = false
     OutsideKGorge.soundName = "kgorgeextended.mp3"
     UIApplication.sharedApplication().scheduleLocalNotification(OutsideKGorge)
     
     let MilfordAvalanche = UILocalNotification()
     MilfordAvalanche.alertBody = "Do not stop within this high avalanche risk area."
-    MilfordAvalanche.regionTriggersOnce = false
+    MilfordAvalanche.regionTriggersOnce = true
     MilfordAvalanche.soundName = "avalanche.mp3"
     MilfordAvalanche.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -44.775014, longitude: 168.006457), radius: 10000.0, identifier: "Location4")
-    MilfordAvalanche.region?.notifyOnExit = false
+    //MilfordAvalanche.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(MilfordAvalanche)
     
     let HomerTunnel = UILocalNotification()
     HomerTunnel.alertBody = "Turn on your lights when inside the homer tunnel."
-    HomerTunnel.regionTriggersOnce = false
+    HomerTunnel.regionTriggersOnce = true
     HomerTunnel.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -44.764211, longitude: 167.981193), radius: 1000.0, identifier: "Location5")
-    HomerTunnel.region?.notifyOnExit = false
+    //HomerTunnel.region.notifyOnExit = false
     HomerTunnel.soundName = "homer.mp3"
     UIApplication.sharedApplication().scheduleLocalNotification(HomerTunnel)
     
     let MilfordRoad = UILocalNotification()
     MilfordRoad.alertBody = "You have entered a high risk zone (Milford Road) , proceed with caution. There is no fuel beyond this point."
-    MilfordRoad.regionTriggersOnce = false
+    MilfordRoad.regionTriggersOnce = true
     MilfordRoad.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -45.406867, longitude: 167.746012), radius: 1000.0, identifier: "Location6")
-    MilfordRoad.region?.notifyOnExit = false
+    //MilfordRoad.region.notifyOnExit = false
     MilfordRoad.soundName = "milfordroadshortened.mp3"
     UIApplication.sharedApplication().scheduleLocalNotification(MilfordRoad)
     
     let DevilsStaircase = UILocalNotification()
     DevilsStaircase.alertBody = "You have entered a high risk zone (Devils Staircase) , proceed with caution"
-    DevilsStaircase.regionTriggersOnce = false
+    DevilsStaircase.regionTriggersOnce = true
     DevilsStaircase.soundName = "devilsstaircase.mp3"
     DevilsStaircase.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -45.199407, longitude: 168.742583), radius: 10000.0, identifier: "Location7")
-    DevilsStaircase.region?.notifyOnExit = false
+    //DevilsStaircase.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(DevilsStaircase)
     
     let DesertRoad = UILocalNotification()
     DesertRoad.alertBody = "You have entered a high risk zone (Desert Road) , proceed with caution."
-    DesertRoad.regionTriggersOnce = false
+    DesertRoad.regionTriggersOnce = true
     DesertRoad.soundName = "desertroad.mp3"
     DesertRoad.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -39.267437, longitude: 175.736685), radius: 25000.0, identifier: "Location8")
-    DesertRoad.region?.notifyOnExit = false
+   // DesertRoad.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(DesertRoad)
     
     let HaurakiPlains = UILocalNotification()
     HaurakiPlains.alertBody = "You have entered a high risk zone (Hauraki Plains) , proceed with caution."
-    HaurakiPlains.regionTriggersOnce = false
+    HaurakiPlains.regionTriggersOnce = true
     HaurakiPlains.soundName = "haurakiplains.mp3"
     HaurakiPlains.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -37.246316, longitude: 175.199842), radius: 12000.0, identifier: "Location9")
-    HaurakiPlains.region?.notifyOnExit = false
+    //HaurakiPlains.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(HaurakiPlains)
     
     let KaimaiRanges = UILocalNotification()
     KaimaiRanges.alertBody = "You have entered a high risk zone (Kaimai Ranges) , proceed with caution."
-    KaimaiRanges.regionTriggersOnce = false
+    KaimaiRanges.regionTriggersOnce = true
     KaimaiRanges.soundName = "kaimai.mp3"
     KaimaiRanges.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -37.825720, longitude: 175.996133), radius: 10000.0, identifier: "Location10")
-    KaimaiRanges.region?.notifyOnExit = false
+    //KaimaiRanges.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(KaimaiRanges)
     
     let Karangahake = UILocalNotification()
     Karangahake.alertBody = "You have entered a high risk zone (Karangahake Gorge) , proceed with caution."
-    Karangahake.regionTriggersOnce = false
+    Karangahake.regionTriggersOnce = true
     Karangahake.soundName = "karangaheke.mp3"
     Karangahake.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -37.418901, longitude: 175.742572), radius: 3000.0, identifier: "Location11")
-    Karangahake.region?.notifyOnExit = false
+    //Karangahake.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(Karangahake)
 
     let LindisPass = UILocalNotification()
     LindisPass.alertBody = "You have entered a high risk zone (Lindis Pass) , proceed with caution."
-    LindisPass.regionTriggersOnce = false
+    LindisPass.regionTriggersOnce = true
     LindisPass.soundName = "lindispass.mp3"
     LindisPass.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -44.585636, longitude: 169.643528), radius: 37000.0, identifier: "Location12")
-    LindisPass.region?.notifyOnExit = false
+    //LindisPass.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(LindisPass)
     
     let RimutakaHill = UILocalNotification()
     RimutakaHill.alertBody = "You have entered a high risk zone (Rimutaka Hill Road) , proceed with caution."
-    RimutakaHill.regionTriggersOnce = false
+    RimutakaHill.regionTriggersOnce = true
     RimutakaHill.soundName = "rimutakahill.mp3"
     RimutakaHill.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -41.109769, longitude: 175.253011), radius: 5000.0, identifier: "Location13")
-    RimutakaHill.region?.notifyOnExit = false
+    //RimutakaHill.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(RimutakaHill)
     
     let NapierTaupo = UILocalNotification()
     NapierTaupo.alertBody = "You have entered a high risk zone (Napier-Taupo Road) , proceed with caution."
-    NapierTaupo.regionTriggersOnce = false
+    NapierTaupo.regionTriggersOnce = true
     NapierTaupo.soundName = "napiertaupo.mp3"
     NapierTaupo.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -39.117149, longitude: 176.581215), radius: 30000.0, identifier: "Location14")
-    NapierTaupo.region?.notifyOnExit = false
+    //NapierTaupo.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(NapierTaupo)
     
     let DomeValley = UILocalNotification()
     DomeValley.alertBody = "You have entered a high risk zone (Dome Valley) , proceed with caution."
-    DomeValley.regionTriggersOnce = false
+    DomeValley.regionTriggersOnce = true
     DomeValley.soundName = "domevalley.mp3"
     DomeValley.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -36.361306, longitude: 174.606477), radius: 6000.0, identifier: "Location15")
-    DomeValley.region?.notifyOnExit = false
+    //DomeValley.region.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(DomeValley)
     
     let Brynderwyn = UILocalNotification()
     Brynderwyn.alertBody = "You have entered a high risk zone (Brynderwyn Hills) , proceed with caution."
-    Brynderwyn.regionTriggersOnce = false
+    Brynderwyn.regionTriggersOnce = true
     Brynderwyn.soundName = "brynderwyns.mp3"
+    //Brynderwyn.region.notifyOnExit = false
     Brynderwyn.region = CLCircularRegion(center: CLLocationCoordinate2D(latitude:
         -36.069030, longitude: 174.421383), radius: 2600.0, identifier: "Location16")
-    Brynderwyn.region?.notifyOnExit = false
     UIApplication.sharedApplication().scheduleLocalNotification(Brynderwyn)
 
     
